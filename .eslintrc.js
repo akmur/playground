598ff27620284763598eb75494a03a0136ab6486
Alessandro Muraro <akmuraro@gmail.com>
2019-04-18 15:11:57 +0200

    modified eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,6 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:prettier/recommended"
   ],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
   parserOptions: {
     parser: "babel-eslint"
   }

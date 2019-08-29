51a8b97fce6de880cb12aee41dacaa29f5f3c5d8
Alessandro Muraro <akmuraro@gmail.com>
2019-08-29 13:28:19 +0200

    adding react example

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-// your code here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Works :)</h1>
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('app')
+ReactDOM.render(<App />, rootElement)

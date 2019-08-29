2887f3dc85c0cf58a592453121b8e7dace4ff07a
Alessandro Muraro <akmuraro@gmail.com>
2019-08-29 13:25:54 +0200

    cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-class App extends React.Component {
-  message = 'yo'
-
-  render() {
-    return <h1>Hello {this.message}</h1>
-  }
-}
-ReactDOM.render(<App />, document.getElementById('root'))
+// your code here
